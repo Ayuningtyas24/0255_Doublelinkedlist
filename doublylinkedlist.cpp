c4a3078815b835e82b494b3ae1d14406f2d69cd9
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-05-16 08:19:47 +0700

    menambahkan memori untuk node baru dan nilai  untuk atribut noMhs

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -27,5 +27,12 @@ public:
         int nim;
         cout << "\nEnter the roll number of the student: ";
         cin >> nim;
+
+        //Step 1: Allocate memory for new node
+        Node *newNode = new Node();
+
+        //Step 2: Assign value to the date fields
+        newNode->noMhs = nim;
+        
     }
 }
