5745a9af266292cb1f9b6be1f100a1b5977c02da
Ayuningtyas24 <adyahseptiani@gmail.com>
2025-05-16 09:09:57 +0700

    menambahkan hapus node dan pesan

--- doublylinkedlist.cpp
+++ doublylinkedlist.cpp
@@ -122,5 +122,9 @@ public:
             if (current->next != NULL)
                 current->next->prev = current->prev;
         }
+
+        // Step 5: Delete the node
+        delete current;
+        cout << "Record with roll number " << rollNo << "delete" << endl;
     }
 };
